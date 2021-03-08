1de9a4448a63a64c1b37e10c271da252325782bb
“Danny <2470314H@student.gla.ac.uk>
2021-03-08 22:32:41 +0000

    initial urls

---
+++ filmfanatics/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from filmfanatics import views
+
+app_name = 'filmfanatics'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('contact_us/', views.contact_us, name = 'contact_us'),
+    path('genre/<slug:genre_name_slug>/', views.genre, name='genre'),
+    path('sign_up/', views.sign_up, name='sign_up'),
+    path('login/', views.user_login, name='login'),
+    path('my_account/', views.account, name='account'),
+    path('logout/', views.logout, name='logout'),
+    path('trending/', views.trending, name='trending'),
+]
--- wad2_project/urls.py
+++ wad2_project/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+from filmfanatics import views
+
 
 urlpatterns = [
+    path('', views.home, name = 'home'),
+    path('filmfanatics/', include('rango.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
